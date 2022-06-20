0b69991ea2ce59a0ee3d9378285219f51f77b094
Icurra <icurrablade@gmail.com>
2022-06-20 11:35:46 -0700

    added console text

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName = "Landin"
+let lastName = "Washington"
+var age = "23"
 // Exercise 2
-
+let fullName = firstName + " " + lastName;
+console.log(fullName)
 // Exercise 3
+let city = "Apple Valley";
+let pastime = "playing video games";
+let myStory= `Hello, my name is ${fullName}.  I was raised in ${city}. I have always loved ${pastime}.`;
+console.log(myStory)  
